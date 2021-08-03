f48d2510f6ab965fe59c7bafbe25cb974c7eb820
maatanyy <maatanyy@naver.com>
2021-08-03 16:52:00 +0900

    #3.2 manage model fields

--- users/models.py
+++ users/models.py
@@ -5,4 +5,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(default="")
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)  # null은 database에서 blank는 form에서
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)  # default or null
