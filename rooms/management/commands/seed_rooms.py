6cc1566b76d1d9d3754b6f229760165de0979493
maatanyy <maatanyy@naver.com>
2021-09-07 16:54:20 +0900

    #9.4 seed_rooms part two

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,6 @@
 import random
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten  # list [[]] 꺼낼라고
 from django_seed import Seed
 from rooms import models as room_models
 from users import models as user_models
@@ -24,7 +25,7 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
-        print(room_types, all_users)
+        # print(room_types, all_users)
         seeder.add_entity(
             room_models.Room,
             number,
@@ -39,5 +40,14 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"room_photos/{random.randint(1,31)}.webp",
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
