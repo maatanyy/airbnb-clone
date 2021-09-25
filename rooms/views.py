410c944392af89f79e7ff961729c44a111ade514
maatanyy <maatanyy@naver.com>
2021-09-25 18:00:08 +0900

    #11.7 class based views

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,7 @@ from math import ceil
 from django.core import paginator  # 올림함수 import
 from django.shortcuts import render, redirect
 from django.core.paginator import EmptyPage, Paginator
+from django.views.generic import ListView
 from . import models
 
 # from datetime import datetime
@@ -10,17 +11,10 @@ from . import models
 # Create your views here.
 
 
-def all_rooms(request):
-    # return HttpResponse(content=f"<h1>{now}</h1>")
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect(
-            "/"
-        )  # 예외의 경우 home으로 돌려보내줌 ,, except Exception 으로 해도되나 어떤 에러를 어케 처리할지 다룰줄 아는게 중요
+class HomeView(ListView):
+    """HomeView Definition"""
 
-    return render(request, "rooms/home.html", {"page": rooms})
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
