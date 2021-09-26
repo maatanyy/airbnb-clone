2e418f69f35eda7e619e2b5ac81d5d3984713d81
maatanyy <maatanyy@naver.com>
2021-09-26 17:12:49 +0900

    #12.0 urls and arguments

--- rooms/views.py
+++ rooms/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 from django.core.paginator import EmptyPage, Paginator
 from django.views.generic import ListView
 from . import models
-from django.utils import timezone
 
 # from datetime import datetime
 # from django.http import HttpResponse
@@ -21,8 +20,6 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+def room_detail(request, pk):
+    return render(request, "rooms/detail.html")
