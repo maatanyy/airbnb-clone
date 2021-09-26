52a3a27b995de75af685e0995209438cdcb9c01e
maatanyy <maatanyy@naver.com>
2021-09-26 16:32:45 +0900

    #11.8 CBV two

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.core.paginator import EmptyPage, Paginator
 from django.views.generic import ListView
 from . import models
+from django.utils import timezone
 
 # from datetime import datetime
 # from django.http import HttpResponse
@@ -18,3 +19,10 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
