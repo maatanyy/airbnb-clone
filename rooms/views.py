480c77815166adad8f29c5ec67009a76995425d8
maatanyy <maatanyy@naver.com>
2022-01-28 12:36:10 +0900

    #13.7 기존 작성한 폼 지우고 장고 폼 이용

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from math import ceil
 from django.core import paginator  # 올림함수 import
 from django.core.paginator import EmptyPage, Paginator
 from django.views.generic import ListView, DetailView  # ListView 사용하기 위해 추가
-from . import models
+from . import models, forms
 from django.shortcuts import render
 from django_countries import countries
 
@@ -41,93 +41,7 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city", "Anywhere")  # ,뒤에는 default
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    room_types = models.RoomType.objects.all()
-    price = int(request.GET.get("price", 0))
-    guests = int(request.GET.get("guests", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    beds = int(request.GET.get("beds", 0))
-    baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.getlist("amenities")   #하나를 가져오고 싶은게 아니니까 getlist를 쓴다!!
-    s_facilities = request.GET.getlist("facilities")  #하나를 가져오고 싶은게 아니니까 getlist를 쓴다!!
-    instant = bool(request.GET.get("instant", False))   #필터에서 true false로 체크하니까 bool해줬음
-    superhost = bool(request.GET.get("superhost", False))
-
-    form = {"city": city, 
-            "s_country": country, 
-            "s_room_type": room_type, 
-            "price": price, 
-            "guests": guests, 
-            "bedrooms": bedrooms, 
-            "beds": beds, 
-            "baths": baths,
-            "s_amenities" : s_amenities,
-            "s_facilities" : s_facilities,
-            "instant" : instant,
-            "superhost" :superhost,
-            }
-
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
-
-    choices = {"countries": countries, "room_types": room_types, "amenities": amenities, "facilities":facilities}
-
-    filter_args = {}
-
-    if city != "Anywhere":
-        filter_args["city__startswith"] = city
-
-    filter_args["country"] = country
-
-    if room_type != 0:
-        filter_args["room_type__pk"] = room_type   #room_type 은 foreignkey이다
-
-    if price != 0:
-        filter_args["price__lte"] = price
-
-    if guests != 0:
-        filter_args["guests__gte"] = guests
-
-    if bedrooms != 0:
-        filter_args["bedrooms__gte"] = bedrooms
     
-    if beds != 0:
-        filter_args["bedrooms__gte"] = beds
-    
-    if baths != 0:
-        filter_args["baths__gte"] = baths
+    form = forms.SearchForm()
 
-    if instant is True:
-        filter_args["instant_book"] = True
-
-    if superhost is True:
-        filter_args["host__superhost"] = True   
-
-    #if len(s_amenities) > 0:
-     #   for s_amenity in s_amenities:
-        #    filter_args["amenities__pk"] = int(s_amenity)
-    #if len(s_facilities) > 0:
-        #for s_facility in s_facilities:
-            #filter_args["facilities__pk"] = int(s_facility)
-
-
-    rooms = models.Room.objects.filter(**filter_args)
-
-    if len(s_amenities) > 0:
-        for s_amenity in s_amenities: 
-            rooms = rooms.filter(amenities__pk=int(s_amenity))   #이부분 위 주석처리한 것과 잘 비교하자 이렇게하면 예를들어 amenity 2개 고르면
-            # 하나만 해당되느 것도 나왔는데 이렇게 하면 둘다 해당되는 것만 나오게 된다. filter.filter 요런느낌!!
-
-    if len(s_facilities) > 0:
-        for s_facility in s_facilities:
-            rooms = rooms.filter(facilities__pk=int(s_facility))
-
-    return render(
-        request,
-        "rooms/search.html",
-        {**form, **choices, "rooms": rooms}  # 여기서 form+chocies로 하면 안됨 **이용해서 한다
-    )
+    return render(request,"rooms/search.html", {"form" : form})
