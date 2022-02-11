965658ac3fc38e9398548d6a0a017f2c6e029867
maatanyy <maatanyy@naver.com>
2022-02-11 14:05:52 +0900

    #23.2 Room Photos part Two

--- rooms/views.py
+++ rooms/views.py
@@ -152,7 +152,7 @@ class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
 class RoomPhotosView(user_mixins.LoggedInOnlyView, RoomDetail):  #Room의 DetailView라고 생각
 
     model = models.Room
-    template_name = "room_photos.html"
+    template_name = "rooms/room_photos.html"
 
     def get_object(self, queryset = None):
         room = super().get_object(queryset = queryset)
